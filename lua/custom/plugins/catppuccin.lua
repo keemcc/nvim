5b9dcdad1644dc40a2f6f9d039501083c21bc80a
Keehin <ktsmccann@gmail.com>
2026-03-19 10:11:08 -0500

    i dont know

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -3,7 +3,7 @@ return {
   name = 'catppuccin',
   priority = 1000,
   opts = {
-    flavour = 'mocha',
+    flavour = 'frappe',
     transparent_background = true,
   },
   init = function() vim.cmd.colorscheme 'catppuccin' end,
